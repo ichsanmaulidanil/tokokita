f412d380206fa233d1fdec21b689d2f1e7255861
ichsan <danilmaulidanil@gmail.com>
2024-05-21 21:34:09 +0700

    first commit

---
+++ Documents/NetBeansProjects/APLIKASI_BANGUN_DATAR/src/aplikasi/RESERT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class RESERT {
+
+    public RESERT() {
+    }
+    
+}
---
+++ Documents/NetBeansProjects/KONVERSI_SUHU/src/KONVERSI_SUHU.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class KONVERSI_SUHU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
