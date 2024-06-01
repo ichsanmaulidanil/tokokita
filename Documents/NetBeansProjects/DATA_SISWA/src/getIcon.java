3e455f17a4aa9aea9ac6034065f9994f0f11d3ae
ichsan <danilmaulidanil@gmail.com>
2024-06-02 02:07:44 +0700

    first commit

---
+++ Documents/NetBeansProjects/DATA_SISWA/src/getIcon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+class getIcon {
+
+    Object getImage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
